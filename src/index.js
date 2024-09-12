e0ac52203a2efe1fd1ddb2184998c6e2c24b552a
ghvaz <gh.vaz28@gmail.com>
2024-09-12 19:24:55 -0300

    criação dos arquivos

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/*
+Sistema de Gerenciamento de LivrosObjetivo
+Desenvolver um sistema CRUD para gerenciar livros, utilizando Node.js e Mongodb. O sistema deve permitir a criação, leitura, atualização, deleção e busca de livros com base em diferentes critérios.
+realizar a validação do email com regex, e o middleware via cep para o preenchimento do endereço
+
+Livros
+{
+  "title": "Livro Exemplo",
+  "author": "Autor Exemplo",
+  "year": 2021,
+  "genre": "Ficção"
+}
+Autor
+{
+  "name": "Fulano",
+  "address": "Object",
+  "email": teste@teste.com.br,
+}
+*/
---
+++ src/routes/livros.js
@@ -0,0 +1,10 @@
+/* 
+Livros
+{
+  "title": "Livro Exemplo",
+  "author": "Autor Exemplo",
+  "year": 2021,
+  "genre": "Ficção"
+}
+*/
+
